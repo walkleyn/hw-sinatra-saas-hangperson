c75bd5a8d15cc2a7f1f88cadce93d8cdf69d3900
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2015-11-09 22:43:45 +0200

    Unable to get guess cucumber step to pass

--- app.rb
+++ app.rb
@@ -39,7 +39,10 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    if @game.guesses.include?(letter)
+      flash[:message] = "You have already used that letter."
+    end
+    flash[:message] = "Invalid guess" unless @game.guess(letter)
     redirect '/show'
   end
   
