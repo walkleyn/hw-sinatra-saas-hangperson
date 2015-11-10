439faec4cba9714c190e1230171c055f9a04324f
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2015-11-10 21:23:03 +0200

    Cater for repeated guesses

--- app.rb
+++ app.rb
@@ -39,7 +39,11 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-#    flash[:message] = "Empty string, try again" if letter.nil?
+    begin
+      flash[:message] = "You have already used that letter." unless @game.guess(letter)
+    rescue Exception => e
+        flash[:message] = e.message
+    end
 #    p @game
 #    if @game.guesses.include?(letter)
 #      flash[:message] = "You have already used that letter."
